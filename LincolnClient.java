652459711b5cac17b146739747ac2d218eac7ff0
Jonathan Zhao <zhaojonathan99@gmail.com>
2019-11-15 10:06:02 -0800

    Finished basic UI

--- LincolnClient.java
+++ LincolnClient.java
@@ -9,16 +9,54 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
-//Introduce UI for textfield + other
 public class LincolnClient extends Application{
     private Socket clientSocket;
+    private PrintWriter out = null;
     private String username = "";
+
+    private BorderPane mainPane = new BorderPane();
+    private TextField userInput = new TextField();
+    private TextArea userOutput = new TextArea();
+    private Scene mainScene;
+    private Stage mainStage;
+
+    private Thread outputThread = 
+    new Thread(() -> {
+        BufferedReader in = null;
+
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch(Exception e){
+            System.out.println("Reader could not be initialized.");
+        }
+
+        String output;
+
+        //constantly read from output stream
+        while(clientSocket.isConnected() && !clientSocket.isClosed()){
+
+            try{
+                output = in.readLine();
+
+                if(output != null){
+                    output(output);
+                }
+
+            } catch(Exception e){
+                System.out.println("Uh oh! Something went wrong.");
+            }
+
+        }
+
+    });
  
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage mainStage){
+        this.mainStage = mainStage;
+        createScene();
 
         try{
             startClient();
@@ -28,97 +66,93 @@ public class LincolnClient extends Application{
 
     }
 
+    private void createScene(){
+        userInput.setPrefWidth(500);
+        userInput.setPrefHeight(20);
+        userOutput.setPrefWidth(500);
+        userOutput.setPrefHeight(400);
+        userOutput.setEditable(false);
+
+        mainPane.setCenter(userOutput);
+        mainPane.setBottom(userInput);
+        mainScene = new Scene(mainPane, 500, 500);
+        mainStage.setScene(mainScene);
+        mainStage.show();
+    }
+
+    public void stop(){
+
+        try{
+            stopConnection();
+            outputThread.interrupt();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void output(String message){
+        userOutput.appendText("\n" + message);
+    }
+
+    private void clearOutput(){
+        userOutput.clear();
+    }
+
     private void startConnection(String ip, int port) throws Exception{
-		System.out.println("Connecting...");
+        //System.out.println("Connecting");
+		output("Connecting...");
         clientSocket = new Socket(ip, port);
-		System.out.println("Connected");
+        output("Connected to " + clientSocket.getInetAddress());
+        //System.out.println("Connected");
     }
  
     private void stopConnection() throws Exception{
         clientSocket.close();
     }
-	
-	private void startClient() throws Exception{
-        Scanner input = new Scanner(System.in);
-        startConnection("10.186.42.222", 53);
-        System.out.println("Enter 'exit' to exit");
-        System.out.println("Please enter a username (less than 20 characters)");
-        int tries = 0;
 
-        //check for duplicate username as well
-        //make serverside checking
-        //assign random username
-        while(invalidUsername(username)){
+    private void setupClient(){
+        output("Enter 'exit' to exit");
+        output("Please enter a username (less than 20 characters)");
 
-            if(tries != 0){
-                System.out.println("Invalid username");
-            }
+        userInput.setOnAction(e -> {
 
-            username = input.next();
-            tries++;
-        }
-
-        Thread inputThread = 
-        new Thread(() -> {
-            PrintWriter out = null;
-
-            try{
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                out.write(username);
-            } catch(Exception e){
-                System.out.println("Writer could not be initialized.");
-            }
-
-            String response;
-
-            //constantly send messages to server
-            while(clientSocket.isConnected()){
-                response = input.nextLine();
-                //check for valid response (limit length, prevent spam, add delay)
-                out.println(response);
-            }
-
-        });
-
-        Thread outputThread = 
-        new Thread(() -> {
-            BufferedReader in = null;
-
-            try {
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            } catch(Exception e){
-                System.out.println("Reader could not be initialized.");
-            }
-
-            String output;
-
-            //constantly read from output stream
-            while(clientSocket.isConnected()){
+            if(invalidUsername(userInput.getText())){
+                output("Invalid username");
+            } else{
+                clearOutput();
+                username = userInput.getText();
+                mainStage.setTitle(username + "'s Chatroom");
 
                 try{
-                    output = in.readLine();
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println(username);
+                } catch(Exception exception){
+                    System.out.println("Writer could not be initialized.");
+                }
 
-                    if(output != null){
-                        System.out.println(output);
+                outputThread.start();
+
+                userInput.setOnAction(E -> {
+                    String response = userInput.getText();
+
+                    if(clientSocket.isConnected() && !clientSocket.isClosed() && response.length() > 0){
+                        out.println(response);
+                        output(username + ": " + response);
                     }
 
-                } catch(Exception e){
-                    System.out.println("Uh oh! Something went wrong.");
-                }
+                    userInput.clear();
+                });
 
             }
 
+            userInput.clear();
         });
-
-        inputThread.start();
-        outputThread.start();
-        
-        while(clientSocket.isConnected()){
-
-        }
-
-        input.close();
-		stopConnection();
+    }
+	
+	private void startClient() throws Exception{
+        startConnection("10.186.66.95", 53);
+        setupClient();
 	}
     
     private static Boolean invalidUsername(String s){
